67fc68516ed81328a6fd3264f8db24074b14fb4c
Kwek Zheng Hui <zheng.hui94@gmail.com>
2024-03-05 21:17:33 +0800

    mod connection url

--- server.js
+++ server.js
@@ -11,22 +11,30 @@ const port = 4000;
 console.log("MONGO_PASSWORD:", process.env.MONGO_PASSWORD);
 const password = encodeURIComponent(process.env.MONGO_PASSWORD);
 
-// Define the base connection URL for MongoDB
-const baseConnectionUrl =
-  "mongodb+srv://admin:m69Rk1A7c2udBc7j@cluster0.esauzrd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-// Construct the final connection URL based on the environment
+// const connection_url = `mongodb+srv://admin:${password}@cluster0.esauzrd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const connection_url =
-  process.env.NODE_ENV === "production"
-    ? baseConnectionUrl
-    : `mongodb+srv://admin:${password}@cluster0.esauzrd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+  "mongodb+srv://admin:" +
+  password +
+  "@cluster0.esauzrd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// const connection_url =
+//   "mongodb+srv://admin:m69Rk1A7c2udBc7j@cluster0.esauzrd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// // Define the base connection URL for MongoDB
+// const baseConnectionUrl =
+//   "mongodb+srv://admin:m69Rk1A7c2udBc7j@cluster0.esauzrd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// // Construct the final connection URL based on the environment
+// const connection_url =
+//   process.env.NODE_ENV === "production"
+//     ? baseConnectionUrl
+//     : `mongodb+srv://admin:${password}@cluster0.esauzrd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 //Middleware
 app.use(express.json());
 app.use(Cors());
 
 ///DB config - original
-console.log("Connection URL:", connection_url);
+// console.log("Connection URL:", connection_url);
 // mongoose.connect(connection_url, {
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
