710efc164ca6513566259f325a92218b390ad409
unknown <zheng.hui94@gmail.com>
2024-03-05 14:46:30 +0800

    bug fix 5

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const port = 4000;
 
 const password = encodeURIComponent(process.env.MONGO_PASSWORD);
 const connection_url =
-  // "mongodb+srv://admin:m69Rk1A7c2udBc7j@cluster0.esauzrd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-  "mongodb+srv://admin:${password}@cluster0.esauzrd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://admin:m69Rk1A7c2udBc7j@cluster0.esauzrd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// "mongodb+srv://admin:${password}@cluster0.esauzrd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 //Middleware
 app.use(express.json());
