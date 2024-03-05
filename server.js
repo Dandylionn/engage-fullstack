2ab292f8be7892bac59c1f12b90ca81adf6df61d
Kwek Zheng Hui <zheng.hui94@gmail.com>
2024-03-05 21:20:49 +0800

    mod connection url

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port = 4000;
 console.log("MONGO_PASSWORD:", process.env.MONGO_PASSWORD);
 const password = encodeURIComponent(process.env.MONGO_PASSWORD);
 
-const connection_url = `mongodb+srv://admin:${password}@cluster0.esauzrd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const connection_url = `mongodb+srv://admin:${password}@cluster0.esauzrd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // const connection_url =
 //   "mongodb+srv://admin:" +
 //   password +
@@ -23,11 +23,11 @@ const connection_url = `mongodb+srv://admin:${password}@cluster0.esauzrd.mongodb
 // const baseConnectionUrl =
 //   "mongodb+srv://admin:m69Rk1A7c2udBc7j@cluster0.esauzrd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-// // Construct the final connection URL based on the environment
-// const connection_url =
-//   process.env.NODE_ENV === "production"
-//     ? baseConnectionUrl
-//     : `mongodb+srv://admin:${password}@cluster0.esauzrd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Construct the final connection URL based on the environment
+const connection_url =
+  process.env.NODE_ENV === "production"
+    ? baseConnectionUrl
+    : `mongodb+srv://admin:${password}@cluster0.esauzrd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 //Middleware
 app.use(express.json());
